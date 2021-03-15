f184e13ccc220ef13834a8cdcc4fde88281c9523
sungjin <tjdwls3469@gmail.com>
2021-03-15 17:55:39 +0900

    [Feat] 백엔드 개발 환경 구축 - 백엔드 개발 환경 구축
    Former-commit-id: 4a2d4f0365f93b6a140686f3e3a38c9cc58b1780

---
+++ backend/src/main/java/com/ssafy/ssokauth/SsokauthApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssokauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsokauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsokauthApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ssafy/ssokauth/SsokauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssokauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsokauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
