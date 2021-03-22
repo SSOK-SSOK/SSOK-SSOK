17e265ede530f19e04011858fafa83073c32bf7c
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-03-22 23:06:33 +0900

    [Feat] create WelcomePage - 웰컴 페이지를 만들었습니다.
    Former-commit-id: 08fab6e78ca835b46ac490ee968f13fad5a50d4f

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import WelcomePage from "@/views/WelcomePage.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-
+  {
+    path: "/",
+    name: "WelcomePage",
+    component: WelcomePage,
+  },
 ];
 
 const router = new VueRouter({
---
+++ frontend/src/views/WelcomePage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <v-toolbar 
+      color="#040431"
+      :elevation="0"
+      height="100%"
+    >
+      <v-avatar size="75" class="mr-2">
+        <img src="@/assets/images/welcome-icon.png" alt="welcome-icon">
+      </v-avatar>
+      <h1 class="font-color">SSOK SSOK</h1>
+      <v-spacer></v-spacer>
+      <v-btn class="mr-2" icon><v-icon color="white" size="75">mdi-google-controller</v-icon></v-btn>
+    </v-toolbar>
+
+    <div style="height: 100%;">
+      dd
+    </div>
+
+    <!-- <v-card
+      class="d-flex align-center"
+      color="#040431"
+      :elevation="0"
+      max-width="70%"
+    >
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1 font-color">
+          다양한 언어를
+          <br>
+          재밌게 배워보아요!
+        </v-list-item-title>
+      </v-list-item-content>
+      <v-card-actions>
+        <v-btn
+          outlined
+          rounded
+          text
+          color="white"
+        >
+          Button
+        </v-btn>
+      </v-card-actions>
+    </v-card> -->
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: "WelcomePage",
+  methods: {
+  },
+};
+</script>
+
+<style>
+
+</style>
