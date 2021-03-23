0046509eec0c766c87cf84b4239271bd101870f6
chaewon <chechekk0@naver.com>
2021-03-23 21:38:59 +0900

    [Docs] Add Deploy.md
    - HTTP -> HTTPS 로 변경하는 과정을 기록했다.
    
    
    Former-commit-id: 43e647981f11b2305d467ace96bbe9460e5c6d3f

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,14 +2,14 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import MainPage from "@/views/MainPage.vue";
 import SmartCamera from "@/views/SmartCamera.vue";
-import WelcomPage from "@/views/WelcomPage.vue";
+import WelcomPage from "@/views/WelcomePage.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "WelcomPage",
+    name: "WelcomePage",
     component: WelcomPage,
   },
   {
