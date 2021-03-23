4a5db89e2690ce23bbaf18db96d9c8a5b9c94141
chaewon <chechekk0@naver.com>
2021-03-23 11:43:00 +0900

    [Feat] Welcome Page 레이아웃 & 디자인
    - Welcome Page의 레이아웃을 조정했다.
    - 디자인을 적용했다.
    
    - 추후에 수정할 점 : font, 배경 이미지 반응형 조절, 캐릭터 이미지 넣기
    
    
    Former-commit-id: 242dd68f546a183df355d50d03ae83f7c613a3ca

--- frontend/src/views/WelcomPage.vue
+++ frontend/src/views/WelcomPage.vue
@@ -1,47 +1,30 @@
 <template>
-  <div>
+  <v-container pa-0 fluid fill-height bg-img>
     <v-toolbar 
-      color="#040431"
+      style="background-color: rgba( 255, 255, 255, 0 );"
       :elevation="0"
       height="100%"
+      width="100%"
     >
       <v-avatar size="75" class="mr-2">
         <img src="@/assets/images/welcome-icon.png" alt="welcome-icon">
       </v-avatar>
-      <h1 class="font-color">SSOK SSOK</h1>
+      <h1 class="font-color display-2">SSOK SSOK</h1>
       <v-spacer></v-spacer>
       <v-btn class="mr-2" icon @click="moveToMainPage"><v-icon color="white" size="75">mdi-google-controller</v-icon></v-btn>
     </v-toolbar>
 
-    <div style="height: 100%;">
-      dd
-    </div>
-
-    <!-- <v-card
-      class="d-flex align-center"
-      color="#040431"
-      :elevation="0"
-      max-width="70%"
-    >
-      <v-list-item-content>
-        <v-list-item-title class="headline mb-1 font-color">
+    <v-row fluid fill-height align="center" class="mx-7" style="min-height: 85vh;">
+      <div style="width: auto;">
+        <h1 class="font-color display-3">
           다양한 언어를
           <br>
           재밌게 배워보아요!
-        </v-list-item-title>
-      </v-list-item-content>
-      <v-card-actions>
-        <v-btn
-          outlined
-          rounded
-          text
-          color="white"
-        >
-          Button
-        </v-btn>
-      </v-card-actions>
-    </v-card> -->
-  </div>
+        </h1>
+        <v-btn>구글에서 준 디자인</v-btn>
+      </div>
+    </v-row>
+  </v-container>
   
 </template>
 
