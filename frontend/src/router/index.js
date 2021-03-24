b9e72dc26a7d7e1a1a723fb1bd894ded92db619a
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-03-24 15:52:48 +0900

    [Feat] Google Oauth
    - 라우터 문제를 해결했습니다.
    
    
    Former-commit-id: 023b8f806bf1b824927558816cbcb0be0d20e886

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import MainPage from "@/views/MainPage.vue";
 import SmartCamera from "@/views/SmartCamera.vue";
-<<<<<<< HEAD
 import WelcomePage from "@/views/WelcomePage.vue";
-=======
-import WelcomPage from "@/views/WelcomePage.vue";
->>>>>>> ef039cc176d0ebb03a02b6e77739610c950c4852
 
 Vue.use(VueRouter);
 
@@ -14,11 +10,7 @@ const routes = [
   {
     path: "/",
     name: "WelcomePage",
-<<<<<<< HEAD
     component: WelcomePage,
-=======
-    component: WelcomPage,
->>>>>>> ef039cc176d0ebb03a02b6e77739610c950c4852
   },
   {
     path: "/main",
