1d14fd732e3db622cfa9ca59ba4b74c42d8094ba
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-03-28 16:06:39 +0900

    [Feat] design WelcomePage
    - 전체적인 margin값과 padding값을 조정했습니다.
    - toolbar를 수정했습니다.
    - button에 효과를 입혔습니다.
    
    
    Former-commit-id: cff8f682e294b9e80837837112dc82c4f89f3b67

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,7 +20,11 @@ export default {
 };
 </script>
 
-<style lang="scss">
-
-
+<style>
+/* @font-face {
+    font-family: 'CookieRunOTF-Bold';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/CookieRunOTF-Bold00.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+} */
 </style>
