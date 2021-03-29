c1f22dab1fe969e2889642d738b8518c0c88bfb5
chaewon <chechekk0@naver.com>
2021-03-29 21:24:14 +0900

    [Feat] WebRTC
    - MediaRecorder API를 이용한 녹음 기능 구현
    - WebRTC기능 적용
    - 음성파일을 저장하는 방법 찾아볼 예정
    
    
    Former-commit-id: abecb79c20c86c52c0d1ebff6d25b2af36193c86 [formerly b666081d1823ed1a2b115df42c39b9fdfd77b314] [formerly ad90d64d3df2c6ce7734688e01c871ac5edf8ea6 [formerly d9ecae312090fa7121a5763737acaaca9481df3e]] [formerly eb5e2f11b2e92adebfba0a36d76134199a8d6bd5 [formerly 10785c9863ec21ea4cc5819316f035f126c17044]] [formerly 76f67efe1adf962f1dedb1a000e890b8f149f0fc [formerly 721175a9089e81f3290b4442d59e5c77638f7c0b] [formerly f9bcddf6845978032e97df85f8da4b3cadf412eb [formerly 08d543b29c744ec142fcdc2a68c0420a00154cd8]] [formerly eb5e2f11b2e92adebfba0a36d76134199a8d6bd5 [formerly 10785c9863ec21ea4cc5819316f035f126c17044] [formerly dc5823929f4b2d6c3fd800163947eee9fd1ec06f [formerly 43c82238e8ec9f2ddd58408245ca7158c0a8e19c]]]]
    Former-commit-id: 76f67efe1adf962f1dedb1a000e890b8f149f0fc [formerly 721175a9089e81f3290b4442d59e5c77638f7c0b] [formerly f9bcddf6845978032e97df85f8da4b3cadf412eb [formerly 08d543b29c744ec142fcdc2a68c0420a00154cd8]]
    Former-commit-id: 76f67efe1adf962f1dedb1a000e890b8f149f0fc [formerly 721175a9089e81f3290b4442d59e5c77638f7c0b]
    Former-commit-id: 5a7925484c7070b628cd1b26e85d96e1c07a183d [formerly 3416d0c54dd24f299cbae2a6953a284f39492d0c]
    Former-commit-id: d6cf1289638ff0532a985c2ee0d5be2e64126e96

--- frontend/src/views/PlayGame.vue
+++ frontend/src/views/PlayGame.vue
@@ -1,6 +1,12 @@
 <template>
   <v-container>
     <NavBar />
+    <div id="microphone-test">
+        <!-- controls show audio -->
+      <audio ref="recordPlayer" controls src="" />
+      <v-btn @click="startRecord">녹음</v-btn>
+      <v-btn @click="endRecord">중단</v-btn>
+    </div>
   </v-container>
 </template>
 
@@ -8,17 +14,28 @@
 import NavBar from "@/components/NavBar.vue";
 export default {
   name: "PlayGame",
-  data: function () {
-    return {
-
-    }
-  },
   components: {
     NavBar
   },
   methods: {
+    startRecord() {
+      navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+        this.mediaRecorder = new MediaRecorder(stream)
+        this.mediaRecorder.addEventListener('dataavailable', (event) => {
+          //After monitoring the recording, put the recorded audio into the audio tag
+          this.$refs.recordPlayer.src = URL.createObjectURL(event.data)
+        })
+        this.mediaRecorder.start() //start recording
+      }).catch((error) => {
+        this.$alert({ title:'error', text: 'Failed to turn on the microphone! ' + error })
+      })
+    },
+    endRecord() {
+      this.mediaRecorder.stop() //End recording
+    }
   }
 }
+
 </script>
 
 <style>
