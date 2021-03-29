2679d445508c5942dfd5341cd15f63a57bf73dea
이승아 <dltmddk1023@gmail.com>
2021-03-29 22:47:01 +0900

    [Chore] Change SmartCamera path
    - smartcamera1을 삭제하고 기존 smartcamera2의 경로를 변경했습니다.
    
    
    Former-commit-id: 3e4dd17997a6f2a3a896430bab24a99778cdf0d3 [formerly 57abd1f9576a9c26abfa44fbe6e77d076f3693ed] [formerly 3a114d50cf002f4fb982205dc7bd70a05fe293ab [formerly cd4ab92bb6991d87df2b39cb671f3970a452ac91]] [formerly e875a267adae11936b4abb9d94a3c6ef95e7df01 [formerly 0d13ea1ff7049242a2a354f2ae72f8f4eb839d4e]] [formerly 75eabbc11352cab29ed6bb452766f6013f0b871b [formerly b7babdaecbf4dcf373496e864e61a0856a7d1d69] [formerly 07b843faaad2b28f74613aa64a20a7350358bf56 [formerly c779b5320380949ea200fa0439b6d157fb631abf]] [formerly e875a267adae11936b4abb9d94a3c6ef95e7df01 [formerly 0d13ea1ff7049242a2a354f2ae72f8f4eb839d4e] [formerly 06b7ec5d97c308b7d67d0a64f5f0cf67f86954e7 [formerly f1e6e37a3b7465802c22cfad5c51a81ee2292594]]]]
    Former-commit-id: 75eabbc11352cab29ed6bb452766f6013f0b871b [formerly b7babdaecbf4dcf373496e864e61a0856a7d1d69] [formerly 07b843faaad2b28f74613aa64a20a7350358bf56 [formerly c779b5320380949ea200fa0439b6d157fb631abf]]
    Former-commit-id: dcd9b88f859304a525ad907c765cc302a1ceead3 [formerly 689116d1691816b8372ce831bc4f94981d8fa3b3] [formerly 75eabbc11352cab29ed6bb452766f6013f0b871b [formerly b7babdaecbf4dcf373496e864e61a0856a7d1d69]]
    Former-commit-id: 043b12edd1747dac477e6a3cb139921549398651 [formerly a49ddb82af700bc8a5b1136466a3a0846d7398ed]
    Former-commit-id: 39d880f0904e2c310c2b3fa57c1d71af011c2fac

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -1,64 +1,71 @@
 <template>
   <v-container fluid fill-height pa-0>
-    <v-col 
-      xl="6" lg="6" md="6" sm="12" xs="12" 
-      style="height: 100%; padding: 0;"
+    <v-col
+      xl="6"
+      lg="6"
+      md="6"
+      sm="12"
+      xs="12"
+      style="height: 100%; padding: 0"
     >
       <div class="ma-4">
-        <p class="header">메인으로<br>돌아가기</p>
+        <p class="header">메인으로<br />돌아가기</p>
       </div>
       <div class="ma-4">
-        <v-btn icon class="ml-5 mt-5" @click="back"><v-icon style="color: white;" size="90">mdi-play-box</v-icon></v-btn>
+        <v-btn icon class="ml-5 mt-5" @click="back"
+          ><v-icon style="color: white" size="90">mdi-play-box</v-icon></v-btn
+        >
       </div>
       <div>
-        <v-img
-          style="width: 75%"
-          src="@/assets/images/char1.png"
-        ></v-img>
+        <v-img style="width: 75%" src="@/assets/images/char1.png"></v-img>
       </div>
     </v-col>
 
-    <v-col style="height: 100%; padding: 0;">
-      <v-row style="background-color: #f1ca21; height: 50.5%;">
+    <v-col style="height: 100%; padding: 0">
+      <v-row style="background-color: #f1ca21; height: 50.5%">
         <v-container class="pa-0">
           <v-row class="ma-4">
-            <p class="header">카드<br>게임하기</p>
+            <p class="header">카드<br />게임하기</p>
             <v-spacer></v-spacer>
-            <v-btn icon class="mr-5 mt-5" @click.stop="drawer = !drawer"><v-icon style="color: white;" size="90">mdi-menu</v-icon></v-btn>
+            <v-btn icon class="mr-5 mt-5" @click.stop="drawer = !drawer"
+              ><v-icon style="color: white" size="90">mdi-menu</v-icon></v-btn
+            >
           </v-row>
           <v-row class="ma-4">
-            <v-btn icon class="ml-5 mt-5" @click="moveToCG"><v-icon style="color: white;" size="90">mdi-play-box</v-icon></v-btn>
+            <v-btn icon class="ml-5 mt-5" @click="moveToCG"
+              ><v-icon style="color: white" size="90"
+                >mdi-play-box</v-icon
+              ></v-btn
+            >
           </v-row>
         </v-container>
       </v-row>
-      <v-row style="background-color: #3ad0de; height: 51%;">
+      <v-row style="background-color: #3ad0de; height: 51%">
         <v-container class="pa-0">
           <v-row class="ma-4">
-            <p class="header">스마트 카메라<br>게임하기</p>
+            <p class="header">스마트 카메라<br />게임하기</p>
           </v-row>
           <v-row class="ma-4">
-            <v-btn icon class="ml-5 mt-5" @click="moveToSC"><v-icon style="color: white;" size="90">mdi-play-box</v-icon></v-btn>
+            <v-btn icon class="ml-5 mt-5" @click="moveToSC"
+              ><v-icon style="color: white" size="90"
+                >mdi-play-box</v-icon
+              ></v-btn
+            >
           </v-row>
         </v-container>
       </v-row>
     </v-col>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      right
-      dark
-      bottom
-      temporary
-    >
-      <div style="text-align: end;">
-        <v-btn icon><v-icon @click.stop="drawer = !drawer" size="50">mdi-close-box</v-icon></v-btn>
+    <v-navigation-drawer v-model="drawer" absolute right dark bottom temporary>
+      <div style="text-align: end">
+        <v-btn icon
+          ><v-icon @click.stop="drawer = !drawer" size="50"
+            >mdi-close-box</v-icon
+          ></v-btn
+        >
       </div>
       <v-list>
-        <v-list-item-group
-          v-model="group"
-          active-class="text--accent-4"
-        >
+        <v-list-item-group v-model="group" active-class="text--accent-4">
           <v-list-item>
             <v-list-item-title>도움말</v-list-item-title>
           </v-list-item>
@@ -77,22 +84,21 @@
 </template>
 
 <script>
-
 export default {
   name: "MainPage",
   data: () => ({
-      drawer: false,
-      group: null,
-    }),
+    drawer: false,
+    group: null,
+  }),
 
-    watch: {
-      group () {
-        this.drawer = false
-      },
+  watch: {
+    group() {
+      this.drawer = false;
     },
+  },
   methods: {
     moveToSC: function () {
-      this.$router.push({ name: "SmartCamera2" });
+      this.$router.push({ name: "SmartCamera" });
     },
     moveToCG: function () {
       this.$router.push({ name: "CardGame" });
