1b3a36e6011136144e55ce0ad5e38c5e3d20954a
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-03-29 23:10:51 +0900

    [Feat] design MainPage
    - 캐릭터를 바꿨습니다.
    
    
    Former-commit-id: 9b5c65318fb29b3924c178c82ead3abe00d74aa8 [formerly a9bb04bf7cfd622c584af5a66785dd91b73ce6f6] [formerly ee4b24412994da3581cda150e8792fd58a720182 [formerly 6cfb7500a8cf7751553c5bea92abc90e0d83fb68]] [formerly 40f6611879df9f8e02d392feb87ecb939b0afcb8 [formerly e4ae83daf5dbf54a05cb194f2760c2f303e29aff]] [formerly bd79b9484d9c07486ead9423282d94b3b31e9843 [formerly 1a1a3acc5e42fe487e7f94c1020acc1e312d4597] [formerly 1f69798d8c8d0f4dae95b92f27f37ca8f4c64f23 [formerly 336f17f33eac481093c42958d7bb4ce72be64df4]] [formerly 40f6611879df9f8e02d392feb87ecb939b0afcb8 [formerly e4ae83daf5dbf54a05cb194f2760c2f303e29aff] [formerly 1ea64b046505f7c4e8130c69108763b4de868d52 [formerly 2043e503ee0f8390ceaa77cad0b5f40d0b07247e]]]]
    Former-commit-id: bd79b9484d9c07486ead9423282d94b3b31e9843 [formerly 1a1a3acc5e42fe487e7f94c1020acc1e312d4597] [formerly 1f69798d8c8d0f4dae95b92f27f37ca8f4c64f23 [formerly 336f17f33eac481093c42958d7bb4ce72be64df4]]
    Former-commit-id: bd79b9484d9c07486ead9423282d94b3b31e9843 [formerly 1a1a3acc5e42fe487e7f94c1020acc1e312d4597]
    Former-commit-id: 5c6bdf216985609b8ccef8ff65a7c37f7c710485 [formerly b73aa57a07269ec195a4f22556f663317626d032]
    Former-commit-id: 13c481943e5f27da8ccc6b0de75adb8df19a5a8f

--- frontend/src/views/MainPage.vue
+++ frontend/src/views/MainPage.vue
@@ -11,10 +11,7 @@
         <v-btn icon class="ml-5 mt-5" @click="back"><v-icon style="color: white;" size="90">mdi-play-box</v-icon></v-btn>
       </div>
       <div>
-        <v-img
-          style="width: 75%"
-          src="@/assets/images/char1.png"
-        ></v-img>
+        <img src="@/assets/images/purple-note.png" alt="purple-note-mascot"></img>
       </div>
     </v-col>
 
@@ -100,3 +97,51 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.container{
+  .col{
+    div{
+      p{
+
+      }
+    }
+    div{
+      .v-btn{
+        .v-btn__content{
+          .v-icon{
+
+          }
+        }
+      }
+    }
+    div{
+      .v-btn{
+        .v-btn__content{
+          .v-icon{
+
+          }
+        }
+      }
+    }
+    div{
+      img{
+
+        width: 85%;
+      }
+    }
+  }
+  .col{
+    .row{
+      .container{
+        .row{
+          .header{
+
+          }
+        }
+      }
+    }
+
+  }
+}
+</style>
