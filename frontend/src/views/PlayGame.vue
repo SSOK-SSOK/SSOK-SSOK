032d5999b1c5d92a9682827cfe0b1f273d67f6ae
chaewon <chechekk0@naver.com>
2021-03-30 12:01:21 +0900

    [Feat] mediaRecorder
    - WebRTC 중 mediaRecorder를 구현했다.
    - Blob 형태로 저장되는 음성데이터를 확인했다.
    
    
    Former-commit-id: dab534094739156cb445e9af55e6a0a8348f69fe [formerly 6392b19393c13d7a54a65a1b12ddfe9a91909dd0] [formerly 7e2e1c489b3aae3d73c5b880ab9538c7bee0e764 [formerly 8b655a0309c98e3b890ac821df856a67eb014f4f]] [formerly 12f238af8f77df0000090136fcb62aa17e1fb852 [formerly 56d2fb901148d6eb8d2a9dabdc81ba1dad811843] [formerly 6e45a839122558e039e8a20f3fefe88da399e1bb [formerly 39b3ef8e2331dbc6bbcc81eb3b56a18d0b2d1993]]] [formerly 746521a55554a773d6e1cf0713e96ff94179b070 [formerly 6719dc279aad9dd513c5f50c47e1073a19fbbd72] [formerly ea4026c66f711893dd5c10fd84f6aae24952438f [formerly fdc979531980acc2efc369dfca5bff4067edd8fc]] [formerly 6e45a839122558e039e8a20f3fefe88da399e1bb [formerly 39b3ef8e2331dbc6bbcc81eb3b56a18d0b2d1993] [formerly 7ccc017ac41bed8d26470154e5ff60e90028042f [formerly e0abd8fc6241d5f2360101a0013a10b08cf1945e]]]]
    Former-commit-id: 746521a55554a773d6e1cf0713e96ff94179b070 [formerly 6719dc279aad9dd513c5f50c47e1073a19fbbd72] [formerly ea4026c66f711893dd5c10fd84f6aae24952438f [formerly fdc979531980acc2efc369dfca5bff4067edd8fc]]
    Former-commit-id: 3946b0db88a092d0e79e6c47c018a8b596ff15d6 [formerly 7be605affdc260ef2a7014cb91b2abea7006549a] [formerly 746521a55554a773d6e1cf0713e96ff94179b070 [formerly 6719dc279aad9dd513c5f50c47e1073a19fbbd72]]
    Former-commit-id: 2976df32fc5624d1fd9f755a99dd415a9d0f7554 [formerly d6bef6a1a3a4e72223d20d7b1594341f839b757a]
    Former-commit-id: e508deed4a2e923067d9a6ffb99e7a18d1385842

--- frontend/src/views/PlayGame.vue
+++ frontend/src/views/PlayGame.vue
@@ -2,7 +2,6 @@
   <v-container>
     <NavBar />
     <div id="microphone-test">
-        <!-- controls show audio -->
       <audio ref="recordPlayer" controls src="" />
       <v-btn @click="startRecord">녹음</v-btn>
       <v-btn @click="endRecord">중단</v-btn>
@@ -12,26 +11,41 @@
 
 <script>
 import NavBar from "@/components/NavBar.vue";
+import axios from 'axios';
 export default {
   name: "PlayGame",
   components: {
     NavBar
   },
   methods: {
-    startRecord() {
-      navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-        this.mediaRecorder = new MediaRecorder(stream)
-        this.mediaRecorder.addEventListener('dataavailable', (event) => {
-          //After monitoring the recording, put the recorded audio into the audio tag
-          this.$refs.recordPlayer.src = URL.createObjectURL(event.data)
+    startRecord: function () {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          this.mediaRecorder = new MediaRecorder(stream)
+          this.mediaRecorder.addEventListener('dataavailable', (event) => {
+            //녹음이 끝나면 Blob 객체를 가리키는 URL을 생성해서 audio에 저장
+            this.$refs.recordPlayer.src = URL.createObjectURL(event.data)
+            
+            //Blob 객체 저장
+            let blob = event.data
+            //base64로 변환할 FileReader
+            var reader = new FileReader();
+            // blob -> base64
+            reader.readAsDataURL(blob);
+
+            reader.onloadend = function () {
+              var base64String = reader.result;
+              console.log(base64String)
+            }
+          })
+          this.mediaRecorder.start() //녹음 시작
+        }).catch((error) => {
+          this.$alert({ title:'error', text: 'Failed to turn on the microphone! ' + error })
         })
-        this.mediaRecorder.start() //start recording
-      }).catch((error) => {
-        this.$alert({ title:'error', text: 'Failed to turn on the microphone! ' + error })
-      })
     },
-    endRecord() {
-      this.mediaRecorder.stop() //End recording
+
+    endRecord: function () {
+      this.mediaRecorder.stop() //녹음 중단하면 audio tag에 저장도 멈춤
     }
   }
 }
