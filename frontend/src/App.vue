427e1fa3b4c9331b5608577e61d94c9772309fe2
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-03-31 17:13:55 +0900

    [Chore] 폰트 적용 관련 코드 정리
    - 쿠키런 볼드체를 삭제했습니다. (사유: 웹폰트 적용)
    - 기존에 App.vue에 있던 글씨체 관련 설정을 base.scss로 이동시켰습니다.
    
    
    Former-commit-id: b4025534cfaaf64acb7336325af92e7c4e925e67 [formerly 57a5b1dbfdbfbf278fd7f4ddccda55164b23fcf4] [formerly bdaa7420db96982c7f8b26303f32b4a8bfdd5f62 [formerly 5f5797a75c04939ea1df6019ccaee3b356848e0d]] [formerly d4effbd613a2aabbdf51b116c00a77d5eacd09cd [formerly 579889a62446a674b0a421b32d40f89aaeaf914c]] [formerly 8b05e24a853cdb804f65a0f129a4997eede33584 [formerly b16ae45819256121091cf825dcec4b050a4fe0e8] [formerly 1c9326e741226dcbbf28c65fcbc9c2bb44984359 [formerly 4ed3511bc20d39b13f6d5e212848f9d60e9685e9]] [formerly d4effbd613a2aabbdf51b116c00a77d5eacd09cd [formerly 579889a62446a674b0a421b32d40f89aaeaf914c] [formerly 09c8f1c1477b60f703eb50e8b55d154b7f9934d8 [formerly 754fb1f48e3acfc9cfeb5dbee0b06cfac05ea3bf]]]]
    Former-commit-id: 8b05e24a853cdb804f65a0f129a4997eede33584 [formerly b16ae45819256121091cf825dcec4b050a4fe0e8] [formerly 1c9326e741226dcbbf28c65fcbc9c2bb44984359 [formerly 4ed3511bc20d39b13f6d5e212848f9d60e9685e9]]
    Former-commit-id: 8b05e24a853cdb804f65a0f129a4997eede33584 [formerly b16ae45819256121091cf825dcec4b050a4fe0e8]
    Former-commit-id: 04d144dd19233b0ab9777c072f7ee14f355b5fb5 [formerly 0f6ad9e0489687a75a42a698e1189238a9e693c4]
    Former-commit-id: 085ff023e51f476f11799774a235a76db1a40410

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,36 +16,5 @@ export default {
 </script>
 
 <style>
-@font-face {
-  font-family: "CookieRunOTF-Bold";
-  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_twelve@1.0/CookieRunOTF-Bold00.woff")
-    format("woff");
-  font-weight: normal;
-  font-style: normal;
-}
-@font-face {
-  font-family: "CookieRun-Regular";
-  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/CookieRun-Regular.woff")
-    format("woff");
-  font-weight: normal;
-  font-style: normal;
-}
-@font-face {
-  font-family: "ELAND_Choice_M";
-  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/ELAND_Choice_M.woff")
-    format("woff");
-  font-weight: normal;
-  font-style: normal;
-}
-@font-face {
-  font-family: "ELAND_Choice_L";
-  src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts-20-12@1.0/ELAND_Choice_L.woff")
-    format("woff");
-  font-weight: normal;
-  font-style: normal;
-}
 
-* {
-  font-family: "CookieRun-Regular";
-}
 </style>
