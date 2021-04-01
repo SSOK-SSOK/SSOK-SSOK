ea334aea7f8b9d9faff90abd70249d0b822892fd
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-04-01 22:21:36 +0900

    [Feat] Category 선택 후, 게임하게 되는 페이지를 만들었습니다.
    - 선택된 카테고리 정보를 빽에 넘겨주어 해당 카테고리의 문제들을
      받아왔습니다.
    - 받아온 문제들 보여주고 15초가 지나면, 다른 문제가 보이게 설정했습니다.
    - 초시계를 넣었습니다.
    
    
    Former-commit-id: 8860e5c0843cee72cac3d5b317376db4caa6d565 [formerly 09a8a570a457f81b58a49cdc4711694827081042] [formerly d4e04ee4ff9354909de50e1fa2d651464dad40a0 [formerly c73f2fb53a2b4399f3d15a31e6996ce121a2b51d]] [formerly a2b84d6dfce06960d06d7216fa82feb92bff0ac1 [formerly 1ee31c09b54e83672b5e7cf3a336c152333c7f9b]] [formerly 0939f606f5373a111240f6988944965df7665a9a [formerly 6d5d0846767f7a6115715154053a53c65b9272ad] [formerly bf6efdee2f02df98ac9eaafb7439553cb40637e1 [formerly 5ab61b409526ab06dbad5e481f44d5f488fdfeb5]] [formerly a2b84d6dfce06960d06d7216fa82feb92bff0ac1 [formerly 1ee31c09b54e83672b5e7cf3a336c152333c7f9b] [formerly 0d5a289e23a599225ee57bc2f75cfe82b2627003 [formerly 91789182a55054970dcdd832363d329e8ea3848e]]]]
    Former-commit-id: 0939f606f5373a111240f6988944965df7665a9a [formerly 6d5d0846767f7a6115715154053a53c65b9272ad] [formerly bf6efdee2f02df98ac9eaafb7439553cb40637e1 [formerly 5ab61b409526ab06dbad5e481f44d5f488fdfeb5]]
    Former-commit-id: 3f68ca27ada62e2f81109d461142ee99a82c21f8 [formerly ff2637297a6145d97aca09fd83afbc19b5ce7503] [formerly 0939f606f5373a111240f6988944965df7665a9a [formerly 6d5d0846767f7a6115715154053a53c65b9272ad]]
    Former-commit-id: cddc1d02249ce575fbbcb92bf777f4e70306fc5b [formerly 0235bfef3e2b18f65120781aaf19176516fb4204]
    Former-commit-id: 0abf50dcae2d8ba609db0442b0a62eb402cdccdc

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,17 +3,20 @@ import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import UserStore from "@/store/modules/UserStore.js";
 import SmartCameraStore from "@/store/modules/SmartCameraStore.js";
+import CardGameStore from "@/store/modules/CardGameStore.js";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     UserStore: UserStore,
     SmartCameraStore: SmartCameraStore,
+    CardGameStore: CardGameStore,
   },
   plugins: [
     createPersistedState({
       paths: ["UserStore"],
       paths: ["SmartCameraStore"],
+      paths: ["CardGameStore"],
     }),
   ],
 });
