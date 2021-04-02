efbe9d98540d69e706441796356ccc73ff34b9a2
이승아 <dltmddk1023@gmail.com>
2021-04-02 14:00:11 +0900

    [Feat] Add media folder & create imaged database
    - 배포환경에서 경로로 이미지 저장이 안되어서 db를 따로 생성했습니다.
    
    
    Former-commit-id: 242614008b1839d7e7e6e4dc991cfde2a1b2e647 [formerly f37cef1e1349a35cf47007fff53c53723b4265fe] [formerly 407e6354db7bcc8a1645ff4e6d39ae4ee6895b3f [formerly 3fdf3dbe781228e6375690ba706ed1d8706419f7]] [formerly be9435bd32acd2455e07a83c52079482eee015ac [formerly 8214b053d0c3ca116dcc6ec3e86bc44443e945ce]] [formerly fd5d31ccaa8c992cba727fd57af76451ac7beff9 [formerly a73d0df2acbdec77dbdeffef8b0d6b826d286e7a] [formerly d20c4af1501db178e52eeb49e577666653716c4d [formerly ca59f324a956bee2384618843f14eecd7479646d]] [formerly be9435bd32acd2455e07a83c52079482eee015ac [formerly 8214b053d0c3ca116dcc6ec3e86bc44443e945ce] [formerly bbb6b375137bc9bdab5ba6765e07997762ccff15 [formerly 0e2040bac5d07ba552b80d0443beb76d4023d298]]]]
    Former-commit-id: fd5d31ccaa8c992cba727fd57af76451ac7beff9 [formerly a73d0df2acbdec77dbdeffef8b0d6b826d286e7a] [formerly d20c4af1501db178e52eeb49e577666653716c4d [formerly ca59f324a956bee2384618843f14eecd7479646d]]
    Former-commit-id: fd5d31ccaa8c992cba727fd57af76451ac7beff9 [formerly a73d0df2acbdec77dbdeffef8b0d6b826d286e7a]
    Former-commit-id: 8c79e8449ee2bf6399eb2aceb4f4627dd282534d [formerly 22581c7e492fca8e5145b0041b692a5f333fadee]
    Former-commit-id: 7ccc268445be1c64176bbf23b3fd6a84412fcb64

--- AI/smartcamera/models.py
+++ AI/smartcamera/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+  title = models.CharField(max_length=255)
+  image = models.ImageField(upload_to='images/')
+
+  def __str__(self):
+        return self.title
