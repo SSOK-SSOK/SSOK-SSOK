cac6ec890a38c8c298f592ab580b70c5b7e54823
이승아 <dltmddk1023@gmail.com>
2021-04-05 15:03:51 +0900

    [Feat] new save image file
    -이미지 파일을 저장하는 새로운 방법을 실험합니다.
    
    
    Former-commit-id: 5ccb6f887afd0624a416872065973102e01f21c5 [formerly 7d6af563d1763240d6a35f1e3fe8dfb73521b26a] [formerly a4f78d1691c7d9ce12504ce3f7b34fc17613a31b [formerly 7b20f17b231ef99662434fedffc3b73c0aaf5847]] [formerly 8afdfa89c68a88df2dac8e9837a65f92b0c18fe2 [formerly 705a775011ad9834bcc87eee2ddf89047d5b6349]] [formerly 73d867b13e243ceb603de88681571cffbcab1a79 [formerly 4dd06b74ab8898257ff6d9515e139669031d45e0] [formerly 612a7af89d7d382f3ca87958e9dc9bf745cbbe5e [formerly 2a64c84f3d014a58101fabeb29d265506332760d]] [formerly 8afdfa89c68a88df2dac8e9837a65f92b0c18fe2 [formerly 705a775011ad9834bcc87eee2ddf89047d5b6349] [formerly fc23b6da44b503ee95eb8a0b5fe431118eb78895 [formerly 43245bfd41ccfb887ceb802da2452fb2b0bdcef4]]]]
    Former-commit-id: 73d867b13e243ceb603de88681571cffbcab1a79 [formerly 4dd06b74ab8898257ff6d9515e139669031d45e0] [formerly 612a7af89d7d382f3ca87958e9dc9bf745cbbe5e [formerly 2a64c84f3d014a58101fabeb29d265506332760d]]
    Former-commit-id: 73d867b13e243ceb603de88681571cffbcab1a79 [formerly 4dd06b74ab8898257ff6d9515e139669031d45e0]
    Former-commit-id: 28151001f43d72d2200b6c983f43297a37a93a08 [formerly e678c991eb8edb70086b676346df3c80f722db66]
    Former-commit-id: d8eb82198f3f1ef99c67a2c58bfe731b76d8ad70

--- AI/smartcamera/models.py
+++ AI/smartcamera/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Images(models.Model):
   title = models.CharField(max_length=255)
-  image = models.ImageField(upload_to='images/')
+  image = models.FileField(upload_to='images/', null=True)
 
   def __str__(self):
         return self.title
