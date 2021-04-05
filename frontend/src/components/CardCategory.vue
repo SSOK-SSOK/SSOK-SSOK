a4689a65bf82ee32a3e5ae623efff1e12c6c8a9f
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-04-05 09:10:05 +0900

    [Feat] design toggle button group @ SelectCardGame
    - 언어 선택 버튼을 토글 그룹 버튼으로 변경하였습니다.
    - 선택된 언어의 값을 저장해주었습니다.
    - 선택된 언어를 어떻게 전달해줄지 백엔드와 상의해야합니다.
    
    
    Former-commit-id: 7b6e58249f58faeaad00ea3fc55445b400dc8193 [formerly e9a79f07229181525f2b8cd4c35e38ed4057cb65] [formerly be6d94f82e2419e3239b762e0c117c0f5b2cce93 [formerly 884358515ea23a9c7c634227c1e9248f1683f609]] [formerly 46399d8f108aa743d8d613885a1a74cdf36d2cee [formerly 7230f01d4ec9ca621ccc9db2b32195f880cbf9f7]] [formerly e1788eea9b225c80d978d2e6a278aedeb4a98344 [formerly 73945534bc1e1673ba810dd9679e612341cc8fbc] [formerly b41c2d4b5fe5a277456bcbc0588f10d6adde827f [formerly 7851f0c1670bf9822ebf0c9f67f8c20c5cde5ec7]] [formerly 46399d8f108aa743d8d613885a1a74cdf36d2cee [formerly 7230f01d4ec9ca621ccc9db2b32195f880cbf9f7] [formerly 38c6a6b34d247e2a75b71184bc77bd77108f0751 [formerly e3504843fa57266d096a0d01064d5ffa79deffe5]]]]
    Former-commit-id: e1788eea9b225c80d978d2e6a278aedeb4a98344 [formerly 73945534bc1e1673ba810dd9679e612341cc8fbc] [formerly b41c2d4b5fe5a277456bcbc0588f10d6adde827f [formerly 7851f0c1670bf9822ebf0c9f67f8c20c5cde5ec7]]
    Former-commit-id: 4abd9d114616b3967bc76dfff760f920fcf12ff0 [formerly 3cf1ee1c01a8244bb68e7fe6c250c2626d132dce] [formerly e1788eea9b225c80d978d2e6a278aedeb4a98344 [formerly 73945534bc1e1673ba810dd9679e612341cc8fbc]]
    Former-commit-id: 8e072348a96c11b6ad7d26e505e8aa3c8c4aaa22 [formerly b9f3166c145a1523e2f8ca34c450d60b84fe3f85]
    Former-commit-id: 07461db60f767ec608b7c50096d37e365662f86a

--- frontend/src/components/CardCategory.vue
+++ frontend/src/components/CardCategory.vue
@@ -1,10 +1,14 @@
 <template>
   <v-container fluid fill-height>
-    <div class="languages">
-      <div class="language" v-for="(flag, name) in flags" :key="name">
-        <img @click="checkLanguage(flag)" :src="require(`@/assets/images/flags/${flag.icon}`)">
-      </div>
-    </div>
+    <v-btn-toggle
+      class="languages"
+      v-model="selected_language"
+      mandatory dark color="deep-purple accent-3" group>
+      <v-btn class="language" v-for="(flag, name) in flags" :key="name">
+        {{flag.name}}
+        <!-- <img @click="checkLanguage(flag)" :src="require(`@/assets/images/flags/${flag.icon}`)"> -->
+      </v-btn>
+    </v-btn-toggle>
     <div class="category-field">
       <SpreadCard :category="categories[0]"/>
       <SpreadCard :category="categories[1]"/>
@@ -22,20 +26,20 @@ export default {
   },
   data: function () {
     return {
+      selected_language: undefined,
       flags: [
-        { name: "kr", icon: "kr.png"},
-        { name: "us", icon: "us.png"},
-        { name: "cn", icon: "cn.png"},
-        { name: "jp", icon: "jp.png"},
-        { name: "fr", icon: "fr.png"},
-        { name: "es", icon: "es.png"},
-        { name: "vn", icon: "vn.png"}
+        // { name: "kr", icon: "kr.png"},
+        { id: 1, name: "us", icon: "us.png"},
+        { id: 2, name: "cn", icon: "cn.png"},
+        { id: 3, name: "jp", icon: "jp.png"},
+        { id: 4, name: "vn", icon: "vn.png"},
+        { id: 5, name: "fr", icon: "fr.png"},
+        { id: 6, name: "es", icon: "es.png"}
       ],
       categories: [
         { id: 1, name: "Animal", sub: "동물", path: "https://i.pinimg.com/originals/d6/3c/de/d63cded9d8454a5b57ee8bef31c3ee71.gif"},
         { id: 2, name: "Fruit", sub: "과일", path: "https://acegif.com/wp-content/gifs/apple-8.gif"}
       ],
-      selectedLanguage: '',
     }
   },
   methods: {
@@ -43,6 +47,7 @@ export default {
       this.selectedLanguage = lang.name
     },
     startGame: function (info) {
+      // 언어 내용까지 합해서 넘어가야할 것 같습니다. 
       this.$store.dispatch("CardGameStore/fetchCards", info.id)
       this.$router.push({
         name: "PlayGame",
@@ -68,14 +73,18 @@ export default {
     width: 100%;
     height: 10%;
     margin-bottom: 5%;
+    background: none !important;
     .language{
-      display: flex;
-      justify-content: center;
-      width: 6%;
-      margin: 0 1%;
+      width: 6vw;
+      height: 70vh;
+      margin: 0;
+      overflow: hidden;
       cursor: pointer;
+      background-color: rgba(22, 18, 0, 0.4);
+      box-shadow: 0 2px 3px 0 rgba(151, 150, 146, 0.4);
       img{
         width: 100%;
+        height: auto;
       }
     }
   }
