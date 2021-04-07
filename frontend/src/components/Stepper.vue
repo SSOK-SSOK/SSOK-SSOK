551c1685e6606d79e387555d4028e237be1d145e
이승아 <dltmddk1023@gmail.com>
2021-04-07 23:15:17 +0900

    [Feat] Add Stepper
    - 각 단계별 사진과 설명을 추가하여 게임설명을 완성했습니다.
    
    
    Former-commit-id: eae6a86f145d53bc484a70b1266ca6c2cc4fda55 [formerly 67a28753461b1cddb65293f482ecac7f00c60dda] [formerly 316c32dfd754bc57ae4a29b1bfe03d1dc8707036 [formerly a3c5661e8dc49457e47fd911764b95cc23997176]] [formerly 9d5ce3cd3e3037493ce6f63c8d1db9213fb51d14 [formerly c79b82625d7b87d628340159ebec1f62ed08fb47]] [formerly 505d59674d7c9befd5e571d2e8240b6258b1507b [formerly 83ccd4c10a1550fddd878cda45453dfd7d5ea3bd] [formerly ba131742f58cface6b1dead1f90f158b8eafc774 [formerly 912c69afc7ab609967e80b3904ae033d55a6121f]] [formerly 9d5ce3cd3e3037493ce6f63c8d1db9213fb51d14 [formerly c79b82625d7b87d628340159ebec1f62ed08fb47] [formerly 4527f50c7024994c1269a19e6f79ee5f81d67c11 [formerly e42aa6eb36b63e5e84df138e217bb4471472dc6a]]]]
    Former-commit-id: 505d59674d7c9befd5e571d2e8240b6258b1507b [formerly 83ccd4c10a1550fddd878cda45453dfd7d5ea3bd] [formerly ba131742f58cface6b1dead1f90f158b8eafc774 [formerly 912c69afc7ab609967e80b3904ae033d55a6121f]]
    Former-commit-id: 505d59674d7c9befd5e571d2e8240b6258b1507b [formerly 83ccd4c10a1550fddd878cda45453dfd7d5ea3bd]
    Former-commit-id: 4923fce99f72260e3c93d99075fb771947e2ec66 [formerly ee59f8f8716af0dd8e31cd8ec63f9e2e4255aaca]
    Former-commit-id: 986d9a29781d1fa14dd46dd6819a4205529060ee

--- frontend/src/components/Stepper.vue
+++ frontend/src/components/Stepper.vue
@@ -14,7 +14,9 @@
 
       <v-stepper-items>
         <v-stepper-content step="1">
-          <v-card class="mb-6" color="grey lighten-1" height="300px"></v-card>
+          <v-card class="mb-6" color="grey lighten-1" height="300px">
+            <img src="@/assets/images/step1.jpg" />
+          </v-card>
           <p>🌍 원하는 언어를 선택해주세요</p>
           <div class="d-flex">
             <v-spacer></v-spacer>
@@ -25,29 +27,38 @@
         </v-stepper-content>
 
         <v-stepper-content step="2">
-          <v-card class="mb-6" color="grey lighten-1" height="300px"></v-card>
+          <v-card class="mb-6" color="grey lighten-1" height="300px">
+            <img src="@/assets/images/step2.jpg" />
+          </v-card>
           <p>🔠 [문제보기] 버튼을 눌러 단어를 확인해주세요</p>
           <div class="d-flex">
+            <v-btn outlined color="indigo" @click="e1 = 1"> 이전 </v-btn>
             <v-spacer></v-spacer>
             <v-btn outlined color="indigo" @click="e1 = 3"> 다음 </v-btn>
           </div>
         </v-stepper-content>
 
         <v-stepper-content step="3">
-          <v-card class="mb-6" color="grey lighten-1" height="300px"></v-card>
+          <v-card class="mb-6" color="grey lighten-1" height="300px">
+            <img src="@/assets/images/step3.jpg" />
+          </v-card>
           <p>📷 단어에 맞는 물건을 사진찍어주세요</p>
           <div class="d-flex">
+            <v-btn outlined color="indigo" @click="e1 = 2"> 이전 </v-btn>
             <v-spacer></v-spacer>
             <v-btn outlined color="indigo" @click="e1 = 4"> 다음 </v-btn>
           </div>
         </v-stepper-content>
 
         <v-stepper-content step="4">
-          <v-card class="mb-6" color="grey lighten-1" height="300px"></v-card>
+          <v-card class="mb-6" color="grey lighten-1" height="300px">
+            <img src="@/assets/images/step4.jpg" />
+          </v-card>
           <p>
             🎇 정답을 확인하고 [다시하기] 버튼을 눌러 게임을 마음껏 즐겨보세요
           </p>
           <div class="d-flex">
+            <v-btn outlined color="indigo" @click="e1 = 3"> 이전 </v-btn>
             <v-spacer></v-spacer>
             <v-btn color="warning" @click="playGame"> 게임 시작 </v-btn>
           </div>
@@ -91,6 +102,10 @@ export default {
   }
   v-stepper-step {
     color: deep-purple darken-4;
+  }
+  img {
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
