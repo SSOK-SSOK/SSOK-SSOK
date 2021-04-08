09d1e1cc4a3eb1f9efd25f26c7dd9947f6ae2b40
Hyojin Kim <dev.hyojinkim@gmail.com>
2021-04-08 21:41:25 +0900

    [Fix] add PageNotFound page
    - 404 페이지를 만들었습니다.
    
    
    Former-commit-id: e5f0ec2d74e8f0a7e1edf0cae24db09518af9280 [formerly 762bb9b2e0b0bb76f8665cb38b0218f19584d413] [formerly bb4d347c391338235fd1b5ef9b95e32c01b7cdcd [formerly f48c6aac4ca11f7f19dacfb96620929133d2cac5]] [formerly 07b30896bfb5e81763e1eddec93197f4c3d870e7 [formerly 82cc91ad04511af169d3a373729d6511651583ce]] [formerly 273430f9f343707fbfed760b6c288a49fd3a8f7f [formerly f890431d1bf3adb1a5dc8375ab12539fecf5364b] [formerly 687287f77f59455118f8c15165b358def14a97c7 [formerly a833cf6fced6445bd5e77b381ade44b16d134286]] [formerly 07b30896bfb5e81763e1eddec93197f4c3d870e7 [formerly 82cc91ad04511af169d3a373729d6511651583ce] [formerly 1e2d32c97667288876b5644db76c8fcf874635f2 [formerly 860dba97edfa49dc72ea4ca418e44f540303bf24]]]]
    Former-commit-id: 273430f9f343707fbfed760b6c288a49fd3a8f7f [formerly f890431d1bf3adb1a5dc8375ab12539fecf5364b] [formerly 687287f77f59455118f8c15165b358def14a97c7 [formerly a833cf6fced6445bd5e77b381ade44b16d134286]]
    Former-commit-id: 280e15a9930a253d84f6f02b982fe5c74ec4a9c4 [formerly a9588ed02546b32fb6305ff6b76029863e855e49] [formerly 273430f9f343707fbfed760b6c288a49fd3a8f7f [formerly f890431d1bf3adb1a5dc8375ab12539fecf5364b]]
    Former-commit-id: f93931fe846201a6ca670635e2518a15c12f081f [formerly c1a9a3ea68582771f15d78f8f52555d3f507e135]
    Former-commit-id: 160942b21088d9027d01cf487a4a985dccb67d47

---
+++ exec/frontend/src/views/PageNotFound.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container fluid fill-height>
+    <div class="background">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </div>
+    <div class="contents">
+      <div class="body">
+        <p>페이지를 찾을 수 없습니다.</p>
+        <p>홈페이지로 돌아가려면 아래의 버튼을 눌러주세요.</p>
+        <div @click="moveWelcomePage" class="glow"></div>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'FrontendPagenotfound', 
+  data() {
+    return {   
+    }
+  },
+  methods: {
+    moveWelcomePage() {
+      this.$router.push({ name: "WelcomePage" });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/star.sass";
+@import "@/style/light-button.scss";
+.container {
+  padding: 1%;
+  .background {
+    position: absolute;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+  }
+  .contents {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    .body{
+      p{
+        text-align: center;
+        font-size: 2vw;
+      }
+      .glow{
+        margin: 10% auto 0;
+      }
+    }
+  }
+}
+</style>
